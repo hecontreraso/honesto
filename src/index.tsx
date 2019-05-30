1d48948db87abad6d6dda7a4f4381b9683370e9d
Teb <hecontreraso@gmail.com>
2019-05-30 11:49:23 -0500

    Finishing login styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import "bulma/css/bulma.css";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import "./Login.scss";
 import logo from "./logo.png";
 
-import Button from "components/shared/Button";
-
 const Login: React.FC = () => {
   return (
     <div className="login-component">
       <div className="form-container">
-        <img src={logo} className="logo" />
-        <span>Honesto</span>
-        rayita
-        <Button label="Login with Google" />
+        <img src={logo} className="logo" alt="logo" />
+        <span className="title is-4">Honesto</span>
+        <hr className="divider" />
         <a className="button">Login with Google</a>
       </div>
     </div>
