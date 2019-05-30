f3a99fa51a74679870ac3410080db1037117436c
Teb <hecontreraso@gmail.com>
2019-05-30 12:58:32 -0500

    Adding user photo

--- src/components/shared/NavBar/UserItem/UserItem.tsx
+++ src/components/shared/NavBar/UserItem/UserItem.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./UserItem.scss";
+import photo from "./user-photo.png";
 
 const UserItem: React.FC = () => {
   return (
     <div className="navbar-item user-item">
-      <span className="name">Jane Smith</span>
-      <span className="logout">Logout</span>
+      <img alt="user" src={photo} className="photo" />
+      <div className="text-container">
+        <span className="name">Jane Smith</span>
+        <span className="logout">Logout</span>
+      </div>
     </div>
   );
 };
