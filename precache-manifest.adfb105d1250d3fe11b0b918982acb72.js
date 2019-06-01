a1b97a163da8c25361299d0d577bf4d556bc1c5e
Teb <hecontreraso@gmail.com>
2019-06-01 13:49:39 -0500

    Updates

---
+++ precache-manifest.adfb105d1250d3fe11b0b918982acb72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d3bc7e2ee441c1333161c7e0dfff33",
+    "url": "/honesto/index.html"
+  },
+  {
+    "revision": "bdfb7c6379e4ba49232b",
+    "url": "/honesto/static/css/main.cb5f6a6d.chunk.css"
+  },
+  {
+    "revision": "b7a899c4cf3fd56b766b",
+    "url": "/honesto/static/js/2.af543d55.chunk.js"
+  },
+  {
+    "revision": "bdfb7c6379e4ba49232b",
+    "url": "/honesto/static/js/main.9d7d30ea.chunk.js"
+  },
+  {
+    "revision": "95ffe469a8f10846ada5",
+    "url": "/honesto/static/js/runtime~main.03fbb97a.js"
+  },
+  {
+    "revision": "bcec75c9b5bca1d03cf371d74c5e928a",
+    "url": "/honesto/static/media/background.bcec75c9.png"
+  },
+  {
+    "revision": "2fa8e90724236c108342b6baef64c7a1",
+    "url": "/honesto/static/media/flag-selected.2fa8e907.svg"
+  },
+  {
+    "revision": "9dd30f0abfce1ad578aa2a2fd2535f10",
+    "url": "/honesto/static/media/flag-unselected.9dd30f0a.svg"
+  },
+  {
+    "revision": "0679f3b13c9255ccd6427eed138af5cd",
+    "url": "/honesto/static/media/star-selected.0679f3b1.svg"
+  },
+  {
+    "revision": "457fbba4f72d5f447d8ab80e32c573f4",
+    "url": "/honesto/static/media/star-unselected.457fbba4.svg"
+  }
+]);
